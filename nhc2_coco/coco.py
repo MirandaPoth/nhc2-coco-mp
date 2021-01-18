bd0c58a68c7942e809aa0743d1e8e37ed26931b0
MirandaPoth <69429932+MirandaPoth@users.noreply.github.com>
2021-01-18 10:53:11 +0000

    Update coco.py
    Added 'MP was here' to connected msg

--- nhc2_coco/coco.py
+++ nhc2_coco/coco.py
@@ -100,7 +100,7 @@ class CoCo:
 
         def _on_connect(client, userdata, flags, rc):
             if rc == 0:
-                _LOGGER.info('Connected!')
+                _LOGGER.info('Connected! MP was here 18/01/2021')
                 client.subscribe(self._profile_creation_id + MQTT_TOPIC_SUFFIX_RSP, qos=1)
                 client.subscribe(self._profile_creation_id + MQTT_TOPIC_PUBLIC_RSP, qos=1)
                 client.subscribe(self._profile_creation_id + MQTT_TOPIC_SUFFIX_EVT, qos=1)
