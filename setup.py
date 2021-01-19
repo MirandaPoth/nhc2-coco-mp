880c2b44fc165bb26a395b932e39b0bfe0f950ac
MirandaPoth <69429932+MirandaPoth@users.noreply.github.com>
2021-01-19 11:25:19 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup, find_packages
 setup(name='nhc2-coco',
       version='0.1',
       packages = find_packages(),
-      data_files=[('', ['nhc2_coco/coco_ca.pem'])]
+      package_data={'nhc2_coco': ['coco_ca.pem']},
+      include_package_data=True
       )
