8756948c59bf57171e469ec6d79d1cb5f92681c8
MirandaPoth <69429932+MirandaPoth@users.noreply.github.com>
2021-01-20 13:26:37 +0000

    Update coco_switch.py
    Add support for generic and alloff

--- nhc2_coco/coco_switch.py
+++ nhc2_coco/coco_switch.py
@@ -18,10 +18,18 @@ class CoCoSwitch(CoCoEntity):
         self.update_dev(dev, callback_container)
 
     def turn_on(self):
-        self._command_device_control(self._uuid, KEY_STATUS, VALUE_ON)
+        if extract_property_value_from_device(dev, KEY_BASICSTATE):
+            _LOGGER.info('Turning ON ' + self.name)
+            self._command_device_control(self._uuid, KEY_BASICSTATE, VALUE_ON)
+        else 
+            self._command_device_control(self._uuid, KEY_STATUS, VALUE_ON)
 
     def turn_off(self):
-        self._command_device_control(self._uuid, KEY_STATUS, VALUE_OFF)
+        if extract_property_value_from_device(dev, KEY_BASICSTATE):
+            _LOGGER.info('Turning OFF ' + self.name)
+            self._command_device_control(self._uuid, KEY_BASICSTATE, VALUE_OFF)
+        else 
+            self._command_device_control(self._uuid, KEY_STATUS, VALUE_OFF)
 
     def update_dev(self, dev, callback_container=None):
         has_changed = super().update_dev(dev, callback_container)
@@ -29,7 +37,7 @@ class CoCoSwitch(CoCoEntity):
         # MP 20/01/2021 for generic and alloff, property 'BasicState' says whether it's off or on
         basicstate_value = extract_property_value_from_device(dev, KEY_BASICSTATE)
         if basicstate_value:
-            _LOGGER.info('BasicState of device ' + self.uuid + ' ' + self.name + ' is ' + basicstate_value)
+            _LOGGER.info('BasicState of device ' + self.type + ' ' + self.uuid + ' ' + self.name + ' is ' + basicstate_value)
 
         if ( status_value and self._is_on != (status_value == VALUE_ON) ) or \
             ( basicstate_value and self._is_on != (basicstate_value == VALUE_ON) ):
