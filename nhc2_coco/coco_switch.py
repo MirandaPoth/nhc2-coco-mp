58acf7fcf3fff5cc48e7542c66e74dab435bbd27
MirandaPoth <69429932+MirandaPoth@users.noreply.github.com>
2021-01-20 12:53:25 +0000

    Update coco_switch.py

--- nhc2_coco/coco_switch.py
+++ nhc2_coco/coco_switch.py
@@ -1,7 +1,10 @@
+import logging
+
 from .coco_entity import CoCoEntity
-from .const import KEY_STATUS, VALUE_ON, VALUE_OFF
+from .const import KEY_STATUS, KEY_BASICSTATE, VALUE_ON, VALUE_OFF
 from .helpers import extract_property_value_from_device
 
+_LOGGER = logging.getLogger(__name__)
 
 class CoCoSwitch(CoCoEntity):
 
@@ -24,7 +27,10 @@ class CoCoSwitch(CoCoEntity):
         has_changed = super().update_dev(dev, callback_container)
         status_value = extract_property_value_from_device(dev, KEY_STATUS)
         # MP 20/01/2021 for generic and alloff, property 'BasicState' says whether it's off or on
-        basicstate_value = extract_property_value_from_device(dev, KEY_BASICSTATUS)
+        basicstate_value = extract_property_value_from_device(dev, KEY_BASICSTATE)
+        if basicstate_value:
+            _LOGGER.info('BasicState of device ' + self.uuid + ' ' + self.name + ' is ' + basicstate_value)
+
         if ( status_value and self._is_on != (status_value == VALUE_ON) ) or \
             ( basicstate_value and self._is_on != (basicstate_value == VALUE_ON) ):
             self._is_on = (status_value == VALUE_ON)
