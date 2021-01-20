a221347b0caa415f3cd87694cdd686d2b89ee1d8
MirandaPoth <69429932+MirandaPoth@users.noreply.github.com>
2021-01-20 13:47:34 +0000

    Update coco_switch.py

--- nhc2_coco/coco_switch.py
+++ nhc2_coco/coco_switch.py
@@ -12,37 +12,48 @@ class CoCoSwitch(CoCoEntity):
     def is_on(self):
         return self._is_on
 
+    @property
+    def on_off_property(self):
+        # 'BasicState' for generic and alloff, 'Status' for the rest
+        return self._on_off_property
+
     def __init__(self, dev, callback_container, client, profile_creation_id, command_device_control):
         super().__init__(dev, callback_container, client, profile_creation_id, command_device_control)
         self._is_on = None
+        self._on_off_property = None
         self.update_dev(dev, callback_container)
 
     def turn_on(self):
-        if extract_property_value_from_device(dev, KEY_BASICSTATE):
-            _LOGGER.info('Turning ON ' + self.name)
-            self._command_device_control(self._uuid, KEY_BASICSTATE, VALUE_ON)
-        else: 
-            self._command_device_control(self._uuid, KEY_STATUS, VALUE_ON)
+        self._command_device_control(self._uuid, self._on_off_property, VALUE_ON)
 
     def turn_off(self):
-        if extract_property_value_from_device(dev, KEY_BASICSTATE):
-            _LOGGER.info('Turning OFF ' + self.name)
-            self._command_device_control(self._uuid, KEY_BASICSTATE, VALUE_OFF)
-        else: 
-            self._command_device_control(self._uuid, KEY_STATUS, VALUE_OFF)
+        self._command_device_control(self._uuid, self.on_off_property, VALUE_OFF)
 
     def update_dev(self, dev, callback_container=None):
         has_changed = super().update_dev(dev, callback_container)
         status_value = extract_property_value_from_device(dev, KEY_STATUS)
-        # MP 20/01/2021 for generic and alloff, property 'BasicState' says whether it's off or on
+        # MP 20/01/2021 for generic and alloff, property 'BasicState' says whether it's off or on, not 'Status'
+        if status_value:
+            self._on_off_property = KEY_STATUS
+        else: 
+            self._on_off_property = KEY_BASICSTATE
+        # MP debugging ...
         basicstate_value = extract_property_value_from_device(dev, KEY_BASICSTATE)
         if basicstate_value:
-            _LOGGER.info('BasicState of device ' + self.type + ' ' + self.uuid + ' ' + self.name + ' is ' + basicstate_value)
+            _LOGGER.info('BasicState of device ' + self.model + ' ' + self.uuid + ' ' + self.name + ' is ' + basicstate_value)
 
+        if status_value and self._is_on != (self._on_off_property == VALUE_ON):
+            self._is_on = (status_value == VALUE_ON)
+            has_changed = True
+
+
+        """
         if ( status_value and self._is_on != (status_value == VALUE_ON) ) or \
             ( basicstate_value and self._is_on != (basicstate_value == VALUE_ON) ):
             self._is_on = (status_value == VALUE_ON)
             has_changed = True
+        """
+
         return has_changed
 
     def _update(self, dev):
