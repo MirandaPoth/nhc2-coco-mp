4febc3a1102f978489a952e592f75a084a6edb19
MirandaPoth <69429932+MirandaPoth@users.noreply.github.com>
2021-01-20 13:09:27 +0000

    Separate module for generic and alloff
    Decided that structurally it works better to have a separate module for generic devices. Put alloff in here too and see if it works.

--- nhc2_coco/coco_generic.py
+++ nhc2_coco/coco_generic.py
@@ -1,8 +1,12 @@
+import logging
+
 from .coco_entity import CoCoEntity
-from .const import KEY_STATUS, VALUE_ON, VALUE_OFF
+from .const import KEY_BASICSTATE, VALUE_ON, VALUE_OFF
 from .helpers import extract_property_value_from_device
 
-class CoCoSwitch(CoCoEntity):
+_LOGGER = logging.getLogger(__name__)
+
+class CoCoGeneric(CoCoEntity):
 
     @property
     def is_on(self):
@@ -14,16 +18,21 @@ class CoCoSwitch(CoCoEntity):
         self.update_dev(dev, callback_container)
 
     def turn_on(self):
-        self._command_device_control(self._uuid, KEY_STATUS, VALUE_ON)
+        self._command_device_control(self._uuid, KEY_BASICSTATE, VALUE_ON)
 
     def turn_off(self):
-        self._command_device_control(self._uuid, KEY_STATUS, VALUE_OFF)
+        self._command_device_control(self._uuid, KEY_BASICSTATE, VALUE_OFF)
 
     def update_dev(self, dev, callback_container=None):
         has_changed = super().update_dev(dev, callback_container)
-        status_value = extract_property_value_from_device(dev, KEY_STATUS)
-        if status_value and self._is_on != (status_value == VALUE_ON) :
-            self._is_on = (status_value == VALUE_ON)
+
+        # MP 20/01/2021 for generic and alloff, property 'BasicState' says whether it's off or on
+        basicstate_value = extract_property_value_from_device(dev, KEY_BASICSTATE)
+        if basicstate_value:
+            _LOGGER.info('BasicState of device ' + self.model + ' ' + self.uuid + ' ' + self.name + ' is ' + basicstate_value)
+
+        if basicstate_value and self._is_on != (basicstate_value == VALUE_ON):
+            self._is_on = (basicstate_value == VALUE_ON)
             has_changed = True
         return has_changed
 
