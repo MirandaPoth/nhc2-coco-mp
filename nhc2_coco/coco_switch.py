e679731df59ea5a7d1f7f235e18b6fff84725ca5
MirandaPoth <69429932+MirandaPoth@users.noreply.github.com>
2021-09-17 11:38:50 +0100

    Re-implementing the basicstate stuff
    And adding logging

--- nhc2_coco/coco_switch.py
+++ nhc2_coco/coco_switch.py
@@ -1,7 +1,12 @@
+# MP
+import logging
+
 from .coco_entity import CoCoEntity
-from .const import KEY_STATUS, VALUE_ON, VALUE_OFF, KEY_BASICSTATE
+from .const import KEY_STATUS, VALUE_ON, VALUE_OFF, KEY_BASICSTATE, VALUE_TRIGGERED # MP Added these last two
 from .helpers import extract_property_value_from_device
 
+# MP
+_LOGGER = logging.getLogger(__name__)
 
 class CoCoSwitch(CoCoEntity):
 
@@ -9,13 +14,29 @@ class CoCoSwitch(CoCoEntity):
     def is_on(self):
         return self._is_on
 
+    # MP
+    @property
+    def on_off_property(self):
+        # 'BasicState' for generic and alloff, 'Status' for the rest
+        return self._on_off_property
+
     def __init__(self, dev, callback_container, client, profile_creation_id, command_device_control):
         super().__init__(dev, callback_container, client, profile_creation_id, command_device_control)
         self._is_on = None
+        # MP
+        self._on_off_property = None
         self.update_dev(dev, callback_container)
 
     def turn_on(self):
-        self._command_device_control(self._uuid, KEY_STATUS, VALUE_ON)
+        # ORIINAL: self._command_device_control(self._uuid, KEY_STATUS, VALUE_ON)
+
+        # This gets called from HA when it wants to turn it ON
+        _LOGGER.debug('HA is turning ON ' + self.name)
+        if self.on_off_property == KEY_BASICSTATE:
+            self._command_device_control(self._uuid, self.on_off_property, VALUE_TRIGGERED)
+        else:
+            self._command_device_control(self._uuid, self.on_off_property, VALUE_ON)
+
 
     def turn_off(self):
         self._command_device_control(self._uuid, KEY_STATUS, VALUE_OFF)
@@ -25,12 +46,18 @@ class CoCoSwitch(CoCoEntity):
         status_value = extract_property_value_from_device(dev, KEY_STATUS)
 
         # MP 16-09-2021 (from 20/01/2021) for generic and alloff, property 'BasicState' says whether it's off or on, not 'Status'
+        if status_value:
+            self._on_off_property = KEY_STATUS
+        else: 
+            self._on_off_property = KEY_BASICSTATE
         basicstate_value = extract_property_value_from_device(dev, KEY_BASICSTATE)
 
-        #_LOGGER.debug('For ' + self.name + ', self.on_off_property is ' + self.on_off_property)
-        # MP debugging ...
-        #if basicstate_value:
-        #    _LOGGER.debug('BasicState of device ' + self.model + ' ' + self.uuid + ' ' + self.name + ' is ' + basicstate_value)
+        # MP debugging
+        _LOGGER.debug('For ' + self.name + ', self.on_off_property is ' + self.on_off_property)
+        if basicstate_value:
+            _LOGGER.debug('BasicState of device ' + self.model + ' ' + self.uuid + ' ' + self.name + ' is ' + basicstate_value)
+        else:
+            _LOGGER.debug('Status of device ' + self.model + ' ' + self.uuid + ' ' + self.name + ' is ' + status_value)
 
         # ORIGINAL:
         #if status_value and self._is_on != (status_value == VALUE_ON):
